3a0852771da51f80878040b3381aca8d99c0aaa4
zhiwenyan <zhiwenyan@icloud.com>
2016-12-13 12:16:32 +0800

    SpingMVCAndMyBatis

---
+++ src/com/steven/bean/Items.java
@@ -0,0 +1,26 @@
+package com.steven.bean;
+
+public class Items {
+	private String name;
+	private float price;
+	private String detail;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+}
